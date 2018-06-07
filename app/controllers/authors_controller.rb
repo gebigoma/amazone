139b305c4e85be9c9d8eafc401d65a188aaaa9ea
Tina <tparkmedia@gmail.com>
2018-06-07 12:25:32 -0700

    created all CRUD files

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,23 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
