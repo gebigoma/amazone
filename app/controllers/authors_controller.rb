49fb409cf664d971392ee86fbb5173f77e621e8a
Tina <tparkmedia@gmail.com>
2018-06-17 10:37:16 -0700

    add route, controller, and view for new form

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,19 +5,26 @@ class AuthorsController < ApplicationController
 
   def show
     @author = Author.find(params[:id])
-    @book = Book.find(params[:id])
   end
 
   def new
+    @author = Author.new
   end
 
   def create
+    @author = Author.new
+    @author.name = params[:author][:name]
+    @author.bio = params[:author][:bio]
+    @author.save
+    redirect_to "/authors"
   end
 
   def edit
+    @author = Author.find(params[:id])
   end
 
   def update
+    
   end
 
   def delete
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'authors#index'
   get '/authors' => 'authors#index'
+  get '/authors/new' => 'authors#new', as: :new_author
   get '/authors/:id' => 'authors#show', as: :author
+  post '/authors' => 'authors#create'
+
 
   get '/books' => 'books#index'
   get '/books/:id' => 'books#show'
